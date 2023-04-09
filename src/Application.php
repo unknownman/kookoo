9b35236e056e15d3f1cd3e48b69eedc983a80f99
ali alavi <ali.masoudi.alavi@gmail.com>
2023-04-09 13:41:02 +0330

    fix

---
+++ src/Application.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kookoo\Core;
+
+use Symfony\Component\Dotenv\Dotenv;
+
+/**
+ * Kookoo PHP Framework Application Manager
+ *
+ * This class serves as the main entry point and manager of the Kookoo PHP framework,
+ * responsible for bootstrapping the application and managing its components.
+ *
+ * @package kookoo/core
+ */
+
+namespace kookoo\core;
+
+class Application
+{
+
+    // 
+
+}
